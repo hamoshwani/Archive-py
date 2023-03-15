4cf98b43afa44abca17ebe3e7cb02b4564bf1e0a
shwani <hhamoka3@gmail.com>
2023-03-15 13:47:13 +0300

    Faker User_Agent removed

--- func/request.py
+++ func/request.py
@@ -1,11 +1,20 @@
 import requests
-from fake_useragent import UserAgent
+import random
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
 
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
 def req(url):
+    user_agent_list = [
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1.1 Safari/605.1.15',
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:77.0) Gecko/20100101 Firefox/77.0',
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36',
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:77.0) Gecko/20100101 Firefox/77.0',
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36',
+    ]
     resp=False
-    ua = UserAgent()
-    headers = {'User-Agent': ua.random}
+    ua = random.choice(user_agent_list)
+    headers = {'User-Agent': ua}
     try:
         r = requests.get(url,headers=headers,verify=False)
         resp=r.text
